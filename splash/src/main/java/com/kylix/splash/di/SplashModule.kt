d42d9a1645de219efae51cc7ca26cfd3c8f29d71
KylixEza <kylixeza.dev@gmail.com>
2024-02-17 18:35:09 +0700

    fix(splash): fixed missing viewmodel injection

---
+++ splash/src/main/java/com/kylix/splash/di/SplashModule.kt
@@ -0,0 +1,9 @@
+package com.kylix.splash.di
+
+import com.kylix.splash.ui.SplashViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val splashModule = module {
+    viewModel { SplashViewModel(get()) }
+}
