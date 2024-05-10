ee6531d3bc806dfdda65c1d1c5b93fe50ccfa39f
Kylix Eza S <ksaputra@dopalearn.com>
2024-05-10 20:15:37 +0700

    fix(history): indonesian local

--- history/src/main/java/com/kylix/history/util/GMTTimeFormatter.kt
+++ history/src/main/java/com/kylix/history/util/GMTTimeFormatter.kt
@@ -1,14 +1,15 @@
 package com.kylix.history.util
 
 import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.TimeZone
 
 fun String.toGMT8Format(): String {
-    val sdf = SimpleDateFormat("d MMM yyyy, HH:mm")
+    val sdf = SimpleDateFormat("d MMM yyyy, HH:mm", Locale("in", "ID"))
     sdf.timeZone = TimeZone.getTimeZone("GMT")
     val date = sdf.parse(this)
 
-    val sdfOutput = SimpleDateFormat("d MMM yyyy, HH:mm")
+    val sdfOutput = SimpleDateFormat("d MMM yyyy, HH:mm", Locale("in", "ID"))
     sdfOutput.timeZone = TimeZone.getTimeZone("GMT+8")
 
     return sdfOutput.format(date)
